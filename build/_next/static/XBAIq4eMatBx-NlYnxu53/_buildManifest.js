539defb5b42aaebdb80cea3c39c080a21b8b5740
John Cruz <johncruzag@gmail.com>
2022-12-02 00:35:03 -0500

    Add files via upload

---
+++ build/_next/static/XBAIq4eMatBx-NlYnxu53/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/81eeae6fe070e475.css","static/chunks/pages/index-8374402422182d6c.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
